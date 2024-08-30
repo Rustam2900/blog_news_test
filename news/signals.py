8fc784b1ad0ac470b8138a54c53b3f7425807186
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-08-30 19:57:32 +1000

    add signals

---
+++ news/signals.py
@@ -0,0 +1,36 @@
+from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
+from django.dispatch import receiver
+from .models import Post, Category
+
+
+@receiver(pre_save, sender=Post)
+def post_pre_save(seder, instance, **kwargs):
+    instance.field = instance.field.upper()
+
+
+"""
+
+"""
+
+
+@receiver(post_save, sender=Post)
+def my_handler(sender, instance, created, **kwargs):
+    if created:
+        print(f"{instance} yaratildi")
+
+
+@receiver(pre_delete, sender=Post)
+def my_handler(sender, instance, **kwargs):
+    print(f"{instance} o'chirilmoqda")
+
+
+@receiver(post_delete, sender=Post)
+def my_handler(sender, instance, **kwargs):
+    print(f"{instance} o'chirildi")
+
+
+@receiver(pre_delete, sender=Category)
+def delete_related_objects(sender, instance, **kwargs):
+    related_objects = Post.objects.filter(my_model=instance)
+    related_objects.delete()
+    print(f"{instance} bilan bog'liq obyektlar o'chirildi")
